c70891e4c64833e6843dbf47dd5a21bfec32fe79
Chris Moffat <chris.ec.moffat@gmail.com>
2021-07-22 10:51:35 -0400

    bot running

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var app = express();
-var path = __dirname + "/";
+app.use(express.static(__dirname + "/"));
 
 app.get("/", function (req, res) {
-  res.sendFile(path + "index.html");
+  res.sendFile("index.html");
 });
 
 var server = app.listen(process.env.PORT || 6001, function () {
