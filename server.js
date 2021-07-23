5aac1846bbd821cc61d989cf3d0ef4288f9ac5d4
Chris Moffat <chris.ec.moffat@gmail.com>
2021-07-23 15:51:27 -0400

    more bot pages config

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ var express = require("express");
 var app = express();
 app.use(express.static(__dirname + "/"));
 
-app.get("/", function (req, res) {
-  res.sendFile("index.html");
+app.get("/bot-1", function (req, res) {
+  res.sendFile(__dirname + "/bot-1.html");
+});
+
+app.get("/bot-2", function (req, res) {
+  res.sendFile(__dirname + "/bot-2.html");
+});
+
+app.get("/bot-3", function (req, res) {
+  res.sendFile(__dirname + "/bot-3.html");
 });
 
 var server = app.listen(process.env.PORT || 6001, function () {
